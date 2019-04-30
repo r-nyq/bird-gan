9eacfb6b95f8eae098a60e891e3cc062d622e155
Robert Nyquist <robert.nyq@gmail.com>
2019-04-30 13:37:06 +0200

    Run on GPU

--- src/train.py
+++ src/train.py
@@ -43,11 +43,13 @@ class VisdomLinePlotter(object):
                 update = 'append')
 
 
-batchSize = 16
+cuda = torch.device('cuda')
+
+batchSize = 8
 imageSize = 128
 
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-std=[0.229, 0.224, 0.225])
+normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
+std=[0.5, 0.5, 0.5])
 
 transform = transforms.Compose([
         transforms.Resize((imageSize, imageSize)),
@@ -71,10 +73,12 @@ def weights_init(m):
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
-generator_net = generator.Generator() 
+generator_net = generator.Generator()
+generator_net.cuda()
 generator_net.apply(weights_init)
 
 discriminator_net = discriminator.Discriminator()
+discriminator_net.cuda()
 discriminator_net.apply(weights_init)
 
 criterion = nn.BCELoss()
@@ -93,39 +97,38 @@ iteration_plotter = VisdomLinePlotter(env_name='Train Plots', xlabel='Iteration'
 
 img_id = None
 img_id2 = None
-for epoch in range(200):
+for epoch in range(5000):
     vis.text('Epoc #' + str(epoch))
     
     for i, data in enumerate(dataloader, 0):
         discriminator_net.zero_grad()
         real, _ = data
-        input = Variable(real)
-        target = Variable(torch.ones(input.size()[0]))
-        output = discriminator_net(input)
-        dis_err_real = criterion(output, target)
+        input = Variable(real).cuda()
+        target = Variable(torch.ones(input.size()[0])).cuda()
+        output = discriminator_net(input).cuda()
+        dis_err_real = criterion(output, target).cuda()
         
-        noise = Variable(torch.randn(input.size()[0], 400, 1, 1))
-        fake = generator_net(noise)
-        target = Variable(torch.zeros(input.size()[0]))
-        output = discriminator_net(fake.detach())
-        dis_err_fake = criterion(output, target)
+        noise = Variable(torch.randn(input.size()[0], 400, 1, 1)).cuda()
+        fake = generator_net(noise).cuda()
+        target = Variable(torch.zeros(input.size()[0])).cuda()
+        output = discriminator_net(fake.detach()).cuda()
+        dis_err_fake = criterion(output, target).cuda()
         
-        #if i % 20 == 0:
         dis_err = dis_err_real + dis_err_fake
         dis_err.backward()
         dis_optimizer.step()
         
         generator_net.zero_grad()
-        target = Variable(torch.ones(input.size()[0]))
-        output = discriminator_net(fake)
-        gen_err = criterion(output, target)
+        target = Variable(torch.ones(input.size()[0])).cuda()
+        output = discriminator_net(fake).cuda()
+        gen_err = criterion(output, target).cuda()
         gen_err.backward()
         gen_optimizer.step()
 
         gen_id = iteration_plotter.plot('gen_loss', 'train', 'Generator Loss', i, gen_err.item())
         dis_id = iteration_plotter.plot('dis_loss', 'train', 'Discriminator Loss', i, dis_err.item())
         
-        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f' % (epoch, 200, i, len(dataloader), dis_err.item(), gen_err.item()))
+        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f' % (epoch, 5000, i, len(dataloader), dis_err.item(), gen_err.item()))
         if i % 100 == 0:
             vutils.save_image(real, '%s/real_samples.png' % "./results", normalize = True)
             fake = generator_net(noise)
@@ -133,9 +136,9 @@ for epoch in range(200):
             if img_id:
                 vis.close(win=img_id)
                 vis.close(win=img_id2)
-            img_id = fake_image_vis.images(fake.detach().numpy())
-            img_id2 = fake_image_vis.images(real.detach().numpy())
+            img_id = fake_image_vis.images(fake.detach().cpu().numpy())
+            img_id2 = fake_image_vis.images(real.detach().cpu().numpy())
     vis.close(win=gen_id)
     vis.close(win=dis_id)
-    epoch_plotter.plot('gen_loss', 'train', 'Generator Loss', epoch, gen_err.data.mean())
-    epoch_plotter.plot('dis_loss', 'train', 'Discriminator Loss', epoch, dis_err.data.mean())
+    #epoch_plotter.plot('gen_loss', 'train', 'Generator Loss', epoch, gen_err.data.mean())
+    #epoch_plotter.plot('dis_loss', 'train', 'Discriminator Loss', epoch, dis_err.data.mean())
