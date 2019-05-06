d858927947ec60f4bf5bcd3e29b0cb5efac0d73b
Robert Nyquist <robert.nyq@gmail.com>
2019-05-06 08:41:25 +0200

    Chech if to use GPU or CPU, be able to load and continue models and ad argparse

--- src/train.py
+++ src/train.py
@@ -17,7 +17,18 @@ import plotter
 import generator
 import discriminator
 
-cuda = torch.device('cuda')
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--generator_model", help="Generator model path",
+    type=str)
+parser.add_argument("--discriminator_model", help="Discriminator model path",
+    type=str)
+parser.add_argument("--epochs", help="Amount of Epochs to train the models",
+    type=int, default=25)
+
+args = parser.parse_args()
+
 
 batchSize = 64
 imageSize = 128
@@ -47,21 +58,38 @@ def weights_init(m):
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
-generator_net = generator.Generator()
-generator_net.cuda()
-generator_net.apply(weights_init)
+USE_CUDA = torch.cuda.is_available()
+print(f"Using cuda: {USE_CUDA}")
 
-discriminator_net = discriminator.Discriminator()
-discriminator_net.cuda()
-discriminator_net.apply(weights_init)
-
-criterion = nn.BCELoss()
+if args.generator_model:
+    generator_net = torch.load(args.generator_model)
+    generator_net.eval()
+    if USE_CUDA:
+        generator_net.cuda()
+else:
+    generator_net = generator.Generator()
+    if USE_CUDA:
+        generator_net.cuda()
+    generator_net.apply(weights_init)
 gen_optimizer = optim.Adam(generator_net.parameters(),
                         lr = 0.0002,
                         betas = (0.5, 0.999))
+
+if args.discriminator_model:
+    discriminator_net = torch.load(args.discriminator_model)
+    discriminator_net.eval()
+    if USE_CUDA:
+        discriminator_net.cuda()
+else:
+    discriminator_net = discriminator.Discriminator()
+    if USE_CUDA:
+        discriminator_net.cuda()
+    discriminator_net.apply(weights_init)
 dis_optimizer = optim.Adam(discriminator_net.parameters(),
                         lr = 0.0002,
                         betas = (0.5, 0.999))
+
+criterion = nn.BCELoss()
 
 epoch_plotter = plotter.EpochPlotter(env_name='Train Plots')
 
@@ -70,38 +98,73 @@ vis = Visdom(env='Train Plots')
 
 img_id = None
 img_id2 = None
-for epoch in range(5000):
+for epoch in range(args.epochs):
     text_id = vis.text('Epoc #' + str(epoch))
     epoch_gen_err = []
     epoch_dis_err = []
     for i, data in enumerate(dataloader, 0):
         discriminator_net.zero_grad()
         real, _ = data
-        input = Variable(real).cuda()
-        target = Variable(torch.ones(input.size()[0])).cuda()
-        output = discriminator_net(input).cuda()
-        dis_err_real = criterion(output, target).cuda()
-        
-        noise = Variable(torch.randn(input.size()[0], 400, 1, 1)).cuda()
-        fake = generator_net(noise).cuda()
-        target = Variable(torch.zeros(input.size()[0])).cuda()
-        output = discriminator_net(fake.detach()).cuda()
-        dis_err_fake = criterion(output, target).cuda()
+        input = Variable(real)
+        if USE_CUDA:
+            input = input.cuda()
+
+        target = Variable(torch.ones(input.size()[0]))
+        if USE_CUDA:
+            target = target.cuda()
+
+        output = discriminator_net(input)
+        if USE_CUDA:
+            output = output.cuda()
+
+        dis_err_real = criterion(output, target)
+        if USE_CUDA:
+            dis_err_real = dis_err_real.cuda()
+
+        noise = Variable(torch.randn(input.size()[0], 400, 1, 1))
+        if USE_CUDA:
+            noise = noise.cuda()
+
+        fake = generator_net(noise)
+        if USE_CUDA:
+            fake = fake.cuda()
+
+        target = Variable(torch.zeros(input.size()[0]))
+        if USE_CUDA:
+            target = target.cuda()
+
+        output = discriminator_net(fake.detach())
+        if USE_CUDA:
+            output = output.cuda()
+
+        dis_err_fake = criterion(output, target)
+        if USE_CUDA:
+            dis_err_fake = dis_err_fake.cuda()
         
         dis_err = dis_err_real + dis_err_fake
         dis_err.backward()
         dis_optimizer.step()
         
         generator_net.zero_grad()
-        target = Variable(torch.ones(input.size()[0])).cuda()
-        output = discriminator_net(fake).cuda()
-        gen_err = criterion(output, target).cuda()
+        
+        target = Variable(torch.ones(input.size()[0]))
+        if USE_CUDA:
+            target = target.cuda()
+
+        output = discriminator_net(fake)
+        if USE_CUDA:
+            output = output.cuda()
+
+        gen_err = criterion(output, target)
+        if USE_CUDA:
+            gen_err = gen_err.cuda()
+
         gen_err.backward()
         gen_optimizer.step()
 
         epoch_gen_err.append(gen_err.item())
         epoch_dis_err.append(dis_err.item())
-        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f' % (epoch, 5000, i, len(dataloader), dis_err.item(), gen_err.item()))
+        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f' % (epoch, args.epochs, i, len(dataloader), dis_err.item(), gen_err.item()))
         if i % 100 == 0:
             vutils.save_image(real, '%s/real_samples.png' % "./results", normalize = True)
             fake = generator_net(noise)
